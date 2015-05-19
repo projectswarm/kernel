592ce2fb5dc938d0b9ec42ed34174c97973c067b
Souphis <gbartyzel@hotmail.com>
2015-05-19 23:05:20 +0200

    Minor update

---
+++ Control/PID/pid.c
@@ -0,0 +1,46 @@
+/*
+ * SWARM
+ * System of Wide-Applicable Robotics Modules
+ *
+ * @module Control/PID
+ */
+
+#include <math.h>
+#include "pid.h"
+
+
+void pidcomputing(volatile struct pidparam *pid, uint16_t speed, uint16_t cur_speed){
+
+	//Calculating error signal between step value and current value
+	pid->error=speed-cur_speed;
+
+	//Summing error for calculating integral part of PID controller
+	if(abs(pid->error)>pid->windupband){
+		pid->istate=pid->istate+pid->error;
+	}
+
+	//Calcualting proportional gain
+	uint16_t Up=pid->error*pid->Kp;
+
+	//Calculating integral gain
+	uint16_t Ui=pid->istate*(pid->Kp/(pid->Ti*pid->Ts));
+
+	//Anti Wind-up
+	if(Ui>pid->windupmax){
+		Ui=pid->windupmax;
+	}
+
+	if(Ui<pid->windupmin){
+		Ui=pid->windupmin;
+	}
+
+	//Calculating derivate gain
+	uint16_t Ud=(pid->Kp*pid->Td*pid->Ts)*(pid->error-pid->prev_error);
+
+	//Converting control signal to PWM signal
+	*pid->controlsignal=(Up+Ui+Ud)/100*88.6;
+
+	//Error in previous sample time
+	pid->prev_error=pid->error;
+}
+
---
+++ Control/PID/pid.h
@@ -0,0 +1,26 @@
+/*
+ * SWARM
+ * System of Wide-Applicable Robotics Modules
+ *
+ * @module Control/PID
+ */
+
+#ifndef CONTROL_PID_PID_H
+#define CONTROL_PID_PID_H
+
+	 struct pidparam{
+		uint16_t Kp; //Proportion gain mulitply by 100
+		uint16_t Ti; //Integral gain mulitply by 100
+		uint16_t Td; //Derivate gain mulitply by 100
+		uint16_t Ts; //Inverted sample time (e.g 0.5s is equal to 2s)
+		uint16_t windupmax; //Wind-up upper limit
+		uint16_t windupmin; //Wind-up lower limit
+		uint16_t prev_error; //Previous measured error
+		uint16_t error; //Current error
+		uint16_t windupband; //Integral opreating band
+		uint16_t istate; //Integral summing state
+		float *controlsignal; //Control signal of PID controller
+	};
+
+	 void pidcomputing(volatile struct infopid, uint16_t, uint16_t);
+#endif
