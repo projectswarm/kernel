927d9f02ee39256a5174f4ba11d21c2e906b5038
Tomasz Wrona <tomasz@wrona.me>
2015-05-23 22:07:08 +0200

    LTFC header file

---
+++ Sensor/Color/ltfc.h
@@ -0,0 +1,14 @@
+/*
+ * SWARM
+ * System of Wide-Applicable Robotics Modules
+ *
+ * @module Sensor/Color
+ */
+
+#ifndef SENSOR_COLOR_LTFC_H
+#define SENSOR_COLOR_LTFC_H
+
+void sensor_color_ltfc_init (void);
+void sensor_color_ltfc_read (uint16_t*);
+
+#endif
